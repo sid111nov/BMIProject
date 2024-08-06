05289d78eb1de889d986a2a0b241bd047c2ef37d
sid111nov <siddharth111nov@gmail.com>
2024-08-06 21:00:17 +0530

    it works

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, request, render_template
+from src.pipeline.CustomData import CustomData
+from src.pipeline.prediction_pipeline import PredictionPipeline
+import numpy as np
+from src.utils.logger import logger
+
+app= Flask(__name__)
+
+@app.route('/',methods=['GET','POST'])
+def index():
+    logger.info("Rendering web page")
+    if (request.method=='GET'):
+        
+        return render_template('index.html')
+    else:
+        data=CustomData(
+            gender=request.form.get('gender'),
+            height=request.form.get('height'),
+            weight=request.form.get('weight')           
+
+        )
+        data_df = data.get_data_as_data_frame()
+        
+        predict_pipeline=PredictionPipeline(data_df)
+        bmi  = predict_pipeline.predict()
+        bmi = np.clip(np.round(bmi).astype(int),a_min=0,a_max=5)
+        bmi_result = getBMIMapping(bmi)
+        logger.info(f"Bmi for input features {data_df} is {bmi_result}")
+        return render_template('index.html',results=bmi_result)
+
+
+def getBMIMapping(bmi):
+    bmi_dict = {
+        0:'Extremely Weak',1:'Weak',2:'Normal',3:'Overweight',4:'Obesity',5:'Extreme Obesity'
+    }
+    return bmi_dict[bmi[0]]
+
+
+if __name__=="__main__":
+    
+    app.run(host="0.0.0.0",debug=True)
